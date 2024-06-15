e419e429b8e1ee9805a3b08bc17f6e19ad6ddb39
chengzh <loveandloss@hotmail.com>
2024-06-15 22:46:10 +0800

    test: 删除测试s00/test3, 更名测试s01/test4-->s01/test3, s01/test5-->s01/test4

--- test/s01/test4.cc
+++ test/s01/test4.cc
@@ -1,51 +1,44 @@
-// s01/test4.cc用timerfd实现了一个单次触发的定时器, 这个程序利用Channel
-// 将timerfd的readable事件转发给timerout()函数
-
 #include "net/EventLoop.h"
-#include "net/Channel.h"
-
-#include <sys/timerfd.h>
 
 #include <glog/logging.h>
 
+#include <string>
+
+int cnt = 0;
 reactor::net::EventLoop* g_loop;
 
-void timeout()
+void printTid()
 {
-  LOG(INFO) << "Timeout!";
-  g_loop->quit();
+  LOG(INFO) << "pid = " << getpid() << ", tid = " << reactor::tid();
+  LOG(INFO) << "now " << reactor::timeFormat(reactor::Clock::now());
+}
+
+void print(const std::string& msg)
+{
+  LOG(INFO) << "msg " << reactor::timeFormat(reactor::Clock::now())
+            << " " << msg;
+  if (++cnt == 20) {
+    g_loop->quit();
+  }
 }
 
 int main(int argc, char const *argv[])
 {
   google::InitGoogleLogging(argv[0]);
 
-  LOG(INFO) << "main(): pid = " << getpid() << ", tid = " << reactor::tid();
-
+  printTid();
   reactor::net::EventLoop loop;
   g_loop = &loop;
 
-  int timerfd = ::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
-  if (timerfd < 0) {
-    LOG(ERROR) << "timerfd_create failed, errno: " << errno;
-    return -1;
-  }
-  reactor::net::Channel channel(&loop, timerfd);
-  channel.setReadCallback(timeout);
-  channel.enableReading();
-
-  struct itimerspec howlong;
-  bzero(&howlong, sizeof(howlong));
-  howlong.it_value.tv_sec = 5;
-  int rc = ::timerfd_settime(timerfd, 0, &howlong, NULL);
-  if (rc < 0) {
-    LOG(ERROR) << "timerfd_settime failed, errno: " << errno;
-    return -1;
-  }
+  print("main");
+  loop.runAfter(1000, std::bind(print, "once1"));
+  loop.runAfter(1500, std::bind(print, "once1.5"));
+  loop.runAfter(2500, std::bind(print, "once2.5"));
+  loop.runAfter(3500, std::bind(print, "once3.5"));
+  loop.runEvery(2000, std::bind(print, "every2"));
+  loop.runEvery(3000, std::bind(print, "every3"));
 
   loop.loop();
-
-  ::close(timerfd);
-
-  return 0;
+  print("main loop exits");
+  sleep(1);
 }
